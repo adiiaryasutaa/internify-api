4fe54a547224d4829fcb092127c044b7560eb745
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2024-06-02 18:12:56 +0800

    Update: Add Admin and NotAdmin middlewares

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ use App\Http\Controllers\Api\ReviewController;
 use App\Http\Controllers\Api\RouteFallbackController;
 use App\Http\Controllers\Api\VacancyApplicationController;
 use App\Http\Controllers\Api\VacancyController;
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\NotAdmin;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', LoginController::class)->name('login');
@@ -24,26 +26,31 @@ Route::post('/auth/register', RegisterController::class)->name('register');
 Route::middleware('auth:sanctum')->group(function (): void {
     Route::post('/auth/logout', LogoutController::class)->name('logout');
 
-    Route::prefix('/admin')->name('admin.')->group(function (): void {
-        Route::apiResource('admins', AdminController::class);
-        Route::apiResource('employers', EmployerController::class);
-        Route::apiResource('apprentices', ApprenticeController::class);
-        Route::apiResource('companies', CompanyController::class);
+    Route::prefix('/admin')
+        ->name('admin.')
+        ->middleware(Admin::class)
+        ->group(function (): void {
+            Route::apiResource('admins', AdminController::class);
+            Route::apiResource('employers', EmployerController::class);
+            Route::apiResource('apprentices', ApprenticeController::class);
+            Route::apiResource('companies', CompanyController::class);
+            Route::apiResource('vacancies', VacancyController::class);
+            Route::apiResource('applications', ApplicationController::class);
+            Route::apiResource('reviews', ReviewController::class);
+        });
+
+    Route::middleware(NotAdmin::class)->group(function (): void {
+        Route::apiResource('companies', CompanyController::class)->only(['index', 'show']);
+        Route::apiResource('companies.vacancies', CompanyVacancyController::class)->only(['index']);
+        Route::apiResource('companies.reviews', CompanyReviewController::class)->except(['show']);
+
         Route::apiResource('vacancies', VacancyController::class);
-        Route::apiResource('applications', ApplicationController::class);
-        Route::apiResource('reviews', ReviewController::class);
+        Route::apiResource('vacancies.applications', VacancyApplicationController::class)->except(['update', 'destroy']);
+
+        Route::apiResource('applications', ApplicationController::class)->except(['store', 'destroy']);
+
+        Route::apiResource('reviews', ReviewController::class)->only(['index']);
     });
-
-    Route::apiResource('companies', CompanyController::class)->only(['index', 'show']);
-    Route::apiResource('companies.vacancies', CompanyVacancyController::class)->only(['index']);
-    Route::apiResource('companies.reviews', CompanyReviewController::class)->except(['show']);
-
-    Route::apiResource('vacancies', VacancyController::class);
-    Route::apiResource('vacancies.applications', VacancyApplicationController::class)->except(['update', 'destroy']);
-
-    Route::apiResource('applications', ApplicationController::class)->except(['store', 'destroy']);
-
-    Route::apiResource('reviews', ReviewController::class)->only(['index']);
 });
 
 Route::fallback(RouteFallbackController::class);
